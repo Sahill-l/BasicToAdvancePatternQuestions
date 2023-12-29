9f26caa40844f7d77224163cb6a348ac09eca5ab
Sahil <131724580+Sahill-l@users.noreply.github.com>
2023-12-29 15:03:13 +0530

    Update pattern10.java

--- src/pattern10.java
+++ src/pattern10.java
@@ -1,3 +1,14 @@
+// *      *
+// **    **
+// ***  ***
+// ********
+// ********
+// ***  ***
+// **    **
+// *      *
+
+
+
 public class pattern10 {
     public static void main(String[] args) {
         int row=4;
