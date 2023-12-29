c16a28a039a9f8e2d79a77b8a186839c5a979030
Sahil <131724580+Sahill-l@users.noreply.github.com>
2023-12-29 15:16:19 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
+// **********
+// **********
+// **********
+// **********
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         int row =4;
         int col=10;
 
