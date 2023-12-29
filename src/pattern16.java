dee752f6559d08890a9ac543a8aae2c34c2b6c8a
Sahil <131724580+Sahill-l@users.noreply.github.com>
2023-12-29 14:54:56 +0530

    Update pattern16.java

--- src/pattern16.java
+++ src/pattern16.java
@@ -1,5 +1,9 @@
+//    *   
+//   * *  
+//  *   * 
+// *******
 public class pattern16 {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         int row=4;
         int col=7;
         for (int i=1;i<=row;i++){
